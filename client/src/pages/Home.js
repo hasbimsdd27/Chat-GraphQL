f4dfd9984e8ff514c1ed0cae4315d2fb4b6a2c29
Hasbi Musaddad <musaddadhasbi@gmail.com>
2020-09-21 21:38:51 +0700

    [hasbi] get users

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,70 @@
 import React from "react";
+import { Row, Col, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { useAuthDispatch } from "../context/auth";
+import { gql, useQuery } from "@apollo/client";
 
-export default function Home() {
+const GET_USERS = gql`
+  query getUsers {
+    getUsers {
+      username
+      email
+      createdAt
+    }
+  }
+`;
+
+export default function Home(props) {
+  const dispatch = useAuthDispatch();
+
+  const logout = () => {
+    dispatch({ type: "LOGOUT" });
+    props.history.push("/login");
+  };
+
+  const { loading, data, error } = useQuery(GET_USERS);
+  if (error) {
+    console.log(error);
+  }
+  if (data) {
+    console.log(data);
+  }
+
+  let usersMarkup;
+  if (!data || loading) {
+    usersMarkup = <p>Loading...</p>;
+  } else if (data.getUsers.length === 0) {
+    usersMarkup = <p>No users have join yet...</p>;
+  } else {
+    usersMarkup = data.getUsers.map((item) => (
+      <div key={item.username}>
+        <p>{item.username}</p>
+      </div>
+    ));
+  }
+
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
+    <>
+      <Row className="bg-white justify-content-around mb-1">
+        <Link to="/login">
+          <Button variant="link">Login</Button>
+        </Link>
+        <Link to="/register">
+          <Button variant="link">Register</Button>
+        </Link>
+
+        <Button variant="link" onClick={logout}>
+          Logout
+        </Button>
+      </Row>
+      <Row>
+        <Col xs={4} className="bg-white">
+          {usersMarkup}
+        </Col>
+        <Col xs={8} className="bg-white">
+          <p>Messages</p>
+        </Col>
+      </Row>
+    </>
   );
 }
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -3,13 +3,14 @@ import { Row, Col, Form, Button } from "react-bootstrap";
 import { gql, useLazyQuery } from "@apollo/client";
 import { Link } from "react-router-dom";
 
+import { useAuthDispatch } from "../context/auth";
+
 const LOGIN_USER = gql`
   query login($username: String!, $password: String!) {
     login(username: $username, password: $password) {
       username
-      email
-      createdAt
       token
+      createdAt
     }
   }
 `;
@@ -22,12 +23,14 @@ export default (props) => {
 
   const [errors, setErrors] = useState({});
 
+  const dispatch = useAuthDispatch();
+
   const [loginUser, { loading }] = useLazyQuery(LOGIN_USER, {
     onError: (err) => {
       setErrors(err.graphQLErrors[0].extensions.errors);
     },
     onCompleted: (data) => {
-      localStorage.setItem("token", data.login.token);
+      dispatch({ type: "LOGIN", payload: data.login });
       props.history.push("/");
     },
   });
@@ -36,7 +39,6 @@ export default (props) => {
     e.preventDefault();
     loginUser({ variables });
   };
-
   return (
     <>
       {" "}
